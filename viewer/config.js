270956542b767223d6856d21e9a76d9fa6246251
Marinos <marinosstamatopoulos@gmail.com>
2026-06-12 22:40:32 +0300

    Create config.js

---
+++ viewer/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://meapegkxexcmqoznxuny.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lYXBlZ2t4ZXhjbXFvem54dW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNzM2NTgsImV4cCI6MjA5Njg0OTY1OH0.w78iMnDz4wZPYt-R_r2w-Fho0sSenIxQDsWKUKvzhEA';
